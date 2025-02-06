cd30f8fc30a7b40bcdbed74151e8fc389e7e4fbf
Priit Laes <plaes@plaes.org>
2025-02-06 15:28:14 +0200

    mac: certification: Add support for TxPeriodicityChangeReq

--- lorawan-device/src/mac/certification.rs
+++ lorawan-device/src/mac/certification.rs
@@ -6,16 +6,23 @@ use lorawan::certification::{parse_downlink_certification_messages, DownlinkDUTC
 pub enum Response {
     NoUpdate,
     DutReset,
+    TxPeriodicityChange(Option<u16>),
 }
 
 pub struct Certification {}
 
 impl Certification {
     pub(crate) fn handle_message(&mut self, data: &[u8]) -> Response {
+        use DownlinkDUTCommand::*;
         let messages = parse_downlink_certification_messages(data);
         for message in messages {
             match message {
-                DownlinkDUTCommand::DutResetReq(_) => return Response::DutReset,
+                DutResetReq(..) => return Response::DutReset,
+                TxPeriodicityChangeReq(payload) => {
+                    if let Ok(periodicity) = payload.periodicity() {
+                        return Response::TxPeriodicityChange(periodicity);
+                    }
+                }
             }
         }
         /* TODO: Report.. ? */
